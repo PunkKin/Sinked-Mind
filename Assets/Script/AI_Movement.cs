725a9fd8e5314d7f64c1f99ad71f0cbfa172f198
Ethan Deschamps Pro <146001193+PunkKin@users.noreply.github.com>
2025-05-14 11:36:29 +0200

    L'ami fonctionne

--- Assets/Script/AI_Movement.cs
+++ Assets/Script/AI_Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class AI_Movement : MonoBehaviour
 {
-    [SerializeField] private bool followPlayer;
+    [SerializeField] private bool followPlayer = true;
+    [SerializeField] private bool heldByPlayer;
 
     [SerializeField] private GameObject player;
 
@@ -20,17 +21,58 @@ public class AI_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (followPlayer)
-        {}
-        if (transform.position.x + 0.25f < player.transform.position.x)
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-            Debug.Log("AI Veut aller à droite");
+            Debug.Log("Held");
+            swapToHeld();
         }
-        else if (transform.position.x - 0.25f > player.transform.position.x)
+
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            rb.velocity = new Vector2(speed * -1, rb.velocity.y);
-            Debug.Log("AI Veut aller à Gauche");
+            Debug.Log("Stop");
+            if (followPlayer)
+                followPlayer = false;
+            else
+                followPlayer = true;
+        }
+
+        if (followPlayer && !heldByPlayer)
+            {
+            if (transform.position.x + 0.25f < player.transform.position.x)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else if (transform.position.x - 0.25f > player.transform.position.x)
+            {
+                rb.velocity = new Vector2(speed * -1, rb.velocity.y);
+            }
+        }
+
+        if (heldByPlayer)
+        {
+            this.gameObject.transform.localPosition = new Vector2(0, 0.75f);
+        }
+    }
+
+/*  Currently the object is used on player  */
+
+    public void swapToHeld()
+    {
+        if (heldByPlayer)
+        {
+            heldByPlayer = false;
+            rb.gravityScale = 1;
+            this.gameObject.transform.parent = null;
+            Debug.Log("Not Holden");
+        }
+        else
+        {
+            Debug.Log("holden");
+            heldByPlayer = true;
+            this.gameObject.transform.parent = player.transform;
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(0, 0);
+            this.gameObject.transform.localPosition = new Vector2(0, 0.75f);
         }
     }
 }
